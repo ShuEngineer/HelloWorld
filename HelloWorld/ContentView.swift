986246bfd35bed74658714df03a8b8fb73467ccd
ShuEngineer <supercar110shu@gmail.com>
2019-10-14 20:49:44 +0900

    Initial Commit

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by 伊藤嵩 on 2019/10/14.
+//  Copyright © 2019 Shu Ito. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
